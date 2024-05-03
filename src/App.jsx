63438028d20930d92f0318b5cd75821e528e82d5
Moshfiqur Rahman <moshfiqurrahman37@gmail.com>
2024-05-03 08:17:31 +0600

    Add LeftSidebar component for navigation Import Trending,Favourite,NewRelease,WatchLater,and CommingSoon icons create a left sidebar layout with navigation links

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,25 @@
 import { Fragment } from "react"
 import Header from "./components/Header"
+import LeftSidebar from "./components/LeftSidebar"
+import MovieList from "./components/MovieList"
 
 export default function App() {
   return (
     <Fragment>
-      <Header />
+
+      <body className="dark:bg-body bg-white font-[Sora] dark:text-white text-dark">
+
+        <Header />
+
+        <main>
+        <div className="container grid lg:grid-cols-[218px_1fr] gap-[3.5rem]">
+        <LeftSidebar />
+        <MovieList />
+        </div>
+        </main>
+
+      </body>
+
     </Fragment>
   )
 }
