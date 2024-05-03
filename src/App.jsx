7256ead65b9fb3ab803a522f8525d435e38f8f04
Moshfiqur Rahman <moshfiqurrahman37@gmail.com>
2024-05-03 08:04:11 +0600

    add Header component with navigation

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import { Fragment } from "react"
+import Header from "./components/Header"
 
 export default function App() {
   return (
     <Fragment>
-      
+      <Header />
     </Fragment>
   )
 }
